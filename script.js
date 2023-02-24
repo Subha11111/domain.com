a0ab8b77918d58547c8c89a15fb709c3b9de40d0
Subha Sarkar <106266122+Subha11111@users.noreply.github.com>
2023-02-24 15:31:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,199 @@
+console.log("BTS Songs");
+let songIndex = 0;
+let audioelement = new Audio('Music/butter.mp3');
+let masterPlay = document.getElementById('masterplay');
+let myProgressBar = document.getElementById('myProgressBar');
+let songs = [
+    {songName: "Butter", filePath: "Music/butter.mp3", coverPath: "img/61CNdrgaz8L._SX450_.jpg"},
+]
+
+//play/pause
+masterPlay.addEventListener('click', ()=>{
+    if(audioelement.paused || audioelement.currentTime<=0){
+        audioelement.play();
+    }
+    else{
+        audioelement.pause();
+    }
+})
+
+// 2ndsong
+
+console.log("BTS Songs");
+let songIndex1 = 0;
+let audioelement1 = new Audio('Music/Run.mp3');
+let masterplay1 = document.getElementById('masterplay1');
+let myProgressBar1 = document.getElementById('myProgressBar');
+let songs1 = [
+    {songName: "Dreamers", filePath: "Music/Run.mp3"},
+]
+
+//play/pause
+masterplay1.addEventListener('click', ()=>{
+    if(audioelement1.paused || audioelement1.currentTime<=0){
+        audioelement1.play();
+    }
+    else{
+        audioelement1.pause();
+    }
+})
+
+//3rdsong
+
+console.log("BTS Songs");
+let songIndex3 = 0;
+let audioelement3 = new Audio('Music/dream.mp3');
+let masterplay3 = document.getElementById('masterplay3');
+let songs3 = [
+    {songName: "Dreamers", filePath: "Music/dream.mp3"},
+]
+
+//play/pause
+masterplay3.addEventListener('click', ()=>{
+    if(audioelement3.paused || audioelement3.currentTime<=0){
+        audioelement3.play();
+    }
+    else{
+        audioelement3.pause();
+    }
+})
+
+//4thsong
+
+console.log("BTS Songs");
+let songIndex4 = 0;
+let audioelement4 = new Audio('Music/Namo.mp3');
+let masterplay4 = document.getElementById('masterplay4');
+let songs4 = [
+    {songName: "Dreamers", filePath: "Music/Namo.mp3"},
+]
+
+//play/pause
+masterplay4.addEventListener('click', ()=>{
+    if(audioelement4.paused || audioelement4.currentTime<=0){
+        audioelement4.play();
+    }
+    else{
+        audioelement4.pause();
+    }
+})
+
+//5th song
+
+console.log("BTS Songs");
+let songIndex5 = 0;
+let audioelement5 = new Audio('Music/India.mp3');
+let masterplay5 = document.getElementById('masterplay5');
+let songs5 = [
+    {songName: "Dreamers", filePath: "Music/India.mp3"},
+]
+
+//play/pause
+masterplay5.addEventListener('click', ()=>{
+    if(audioelement5.paused || audioelement5.currentTime<=0){
+        audioelement5.play();
+    }
+    else{
+        audioelement5.pause();
+    }
+})
+
+//6th song
+
+console.log("BTS Songs");
+let songIndex6 = 0;
+let audioelement6 = new Audio('Music/Abhi.mp3');
+let masterplay6 = document.getElementById('masterplay6');
+let songs6 = [
+    {songName: "Dreamers", filePath: "Music/Abhi.mp3"},
+]
+
+//play/pause
+masterplay6.addEventListener('click', ()=>{
+    if(audioelement6.paused || audioelement6.currentTime<=0){
+        audioelement6.play();
+    }
+    else{
+        audioelement6.pause();
+    }
+})
+
+//7th song
+
+console.log("BTS Songs");
+let songIndex7 = 0;
+let audioelement7 = new Audio('Music/Agar.mp3');
+let masterplay7 = document.getElementById('masterplay7');
+let songs7 = [
+    {songName: "Dreamers", filePath: "Music/Agar.mp3"},
+]
+
+//play/pause
+masterplay7.addEventListener('click', ()=>{
+    if(audioelement7.paused || audioelement7.currentTime<=0){
+        audioelement7.play();
+    }
+    else{
+        audioelement7.pause();
+    }
+})
+
+//8th song
+
+console.log("BTS Songs");
+let songIndex8 = 0;
+let audioelement8 = new Audio('Music/Sunn.mp3');
+let masterplay8 = document.getElementById('masterplay8');
+let songs8 = [
+    {songName: "Dreamers", filePath: "Music/Sunn.mp3"},
+]
+
+//play/pause
+masterplay8.addEventListener('click', ()=>{
+    if(audioelement8.paused || audioelement8.currentTime<=0){
+        audioelement8.play();
+    }
+    else{
+        audioelement8.pause();
+    }
+})
+
+//9th song
+
+console.log("BTS Songs");
+let songIndex9 = 0;
+let audioelement9 = new Audio('Music/sanam.mp3');
+let masterplay9 = document.getElementById('masterplay9');
+let songs9 = [
+    {songName: "Dreamers", filePath: "Music/sanam.mp3"},
+]
+
+//play/pause
+masterplay9.addEventListener('click', ()=>{
+    if(audioelement9.paused || audioelement9.currentTime<=0){
+        audioelement9.play();
+    }
+    else{
+        audioelement9.pause();
+    }
+})
+
+//main song
+
+console.log("BTS Songs");
+let songIndex2 = 0;
+let audioelement2 = new Audio('Music/TUSE.mp3');
+let masterplay2 = document.getElementById('masterplay2');
+let songs2 = [
+    {songName: "Dreamers", filePath: "Music/TUSE.mp3"},
+]
+
+//play/pause
+masterplay2.addEventListener('click', ()=>{
+    if(audioelement2.paused || audioelement2.currentTime<=0){
+        audioelement2.play();
+    }
+    else{
+        audioelement2.pause();
+    }
+})
